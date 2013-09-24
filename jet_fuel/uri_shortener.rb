da6223a8ea74f601a554837e349b965f19633fb7
Ryan Vergeront <ryanvergeront@gmail.com>
2013-09-23 20:38:25 -0500

    Never generate duplicate URN

--- jet_fuel/uri_shortener.rb
+++ jet_fuel/uri_shortener.rb
@@ -1,18 +1,32 @@
 require 'securerandom'
 
 class URIShortener
+
   def initialize(short_domain_name, opts={})
     @short_domain_name = short_domain_name
-    @urn_generator = opts.fetch(:urn_generator) { SecureRandom}
+    @urn_generator = opts.fetch(:urn_generator) { ->{ SecureRandom.urlsafe_base64(5) } }
     @shortened_urns = {}
   end
 
   def shorten(uri_to_shorten)
-    short_urn ||= @urn_generator.urlsafe_base64(5)
+
+    short_urn = generate_uniq_urn
     @shortened_urns[short_urn] = uri_to_shorten
+
     short_uri = ""
     short_uri << @short_domain_name
     short_uri << '/'
     short_uri << short_urn
   end
+
+  private
+
+  def generate_uniq_urn
+    begin
+      short_urn = @urn_generator.call
+    end while @shortened_urns.include?(short_urn)
+
+    short_urn
+  end
+
 end
--- spec/jet_fuel/uri_shortener_spec.rb
+++ spec/jet_fuel/uri_shortener_spec.rb
@@ -8,10 +8,20 @@ describe URIShortener do
 
   it "creates unique uri" do
     #TODO think about how to handle same user creating duplicate URN
-    s = URIShortener.new("a.com")
-    expect(s.shorten("x")).not_to eq s.shorten("y")
+    shortener = URIShortener.new("jet.com")
+    expect(shortener.shorten("x")).not_to eq shortener.shorten("y")
   end
-  it "doesn't create duplicate uri if the generator initially creates a dup URN"
+
+  it "doesn't create duplicate uri if the generator initially creates a dup URN" do
+    urn_generator = double
+    urn_generator.stub(:call).and_return("duplicate_urn", "duplicate_urn", "uniq_urn")
+
+    shortener = URIShortener.new("jet.com", urn_generator: urn_generator )
+    first_uri = shortener.shorten(double)
+    second_uri = shortener.shorten(double)
+    expect(second_uri).not_to match /duplicate_urn/
+    expect(second_uri).to match /uniq_urn/
+  end
 
   it "returns the specified URN if given"
 
