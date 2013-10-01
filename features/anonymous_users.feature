e69a958cfb67e51bd36680df206856ea7a7188a8
Ryan Vergeront <ryanvergeront@gmail.com>
2013-10-01 14:05:08 -0400

    Add feature: follow shortened URL as anon user

--- features/anonymous_users.feature
+++ features/anonymous_users.feature
@@ -5,3 +5,9 @@ Feature: Anonymous Users can use the site
     When I visit the site
     And give a URL to the service
     Then I expect it to return a service shortened URL
+
+  Scenario: Shorten URLs redirect anonymous users to the original URL
+    Given that I am an anonymous user of the system
+    When I follow a service shortened URL
+    Then I expect to be redirected to the original URL
+
--- features/step_definitions/anonymous_users_steps.rb
+++ features/step_definitions/anonymous_users_steps.rb
@@ -13,3 +13,14 @@ end
 Then(/^I expect it to return a service shortened URL$/) do
   expect(page).to have_content(/jet\.io\/\w+/)
 end
+
+When(/^I follow a service shortened URL$/) do
+  visit '/'
+  fill_in "url", with: "www.example.com/url/1"
+  click_button "Shorten URL"
+  click_link
+end
+
+Then(/^I expect to be redirected to the original URL$/) do
+  pending # express the regexp above with the code you wish you had
+end
