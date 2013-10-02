ead78979ab266edf6faf93332f266d958aebf5a7
Ryan Vergeront <ryanvergeront@gmail.com>
2013-10-02 10:58:43 -0400

    Update Anon user steps

--- features/step_definitions/anonymous_users_steps.rb
+++ features/step_definitions/anonymous_users_steps.rb
@@ -15,10 +15,10 @@ Then(/^I expect it to return a service shortened URL$/) do
 end
 
 When(/^I follow a service shortened URL$/) do
-  visit '/'
-  fill_in "url", with: "www.example.com/url/1"
-  click_button "Shorten URL"
-  click_link
+  original_uri = "example.com/page"
+  short_uri = URIShortener.new("jet.io").shorten(original_uri)
+  PersistedURI.new(short_uri: short_uri, long_uri: original_uri).save
+  visit short_uri
 end
 
 Then(/^I expect to be redirected to the original URL$/) do
