9d070801a27656379c8de42225f3df920d54371c
Ryan Vergeront <ryanvergeront@gmail.com>
2013-10-02 12:08:02 -0400

    Update anon user steps to find short URL

--- features/step_definitions/anonymous_users_steps.rb
+++ features/step_definitions/anonymous_users_steps.rb
@@ -11,16 +11,17 @@ When(/^give a URL to the service$/) do
 end
 
 Then(/^I expect it to return a service shortened URL$/) do
-  expect(page).to have_content(/jet\.io\/\w+/)
+  expect(page).to have_content(/#{current_url}rd\/\w+/)
 end
 
 When(/^I follow a service shortened URL$/) do
   original_uri = "example.com/page"
-  short_uri = URIShortener.new("jet.io").shorten(original_uri)
-  PersistedURI.new(short_uri: short_uri, long_uri: original_uri).save
-  visit short_uri
+  short_urn = URIShortener.new.shorten(original_uri)
+  PersistedURI.new(short_uri: short_urn, long_uri: original_uri).save
+  visit "/rd/#{short_urn}"
 end
 
 Then(/^I expect to be redirected to the original URL$/) do
-  pending # express the regexp above with the code you wish you had
+  follow_redirect!
+  expect(last_response.url).to eq "www.example.com/url/1"
 end
