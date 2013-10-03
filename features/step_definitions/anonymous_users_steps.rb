2e2bdf01cd8a46a5343e088ca40d0b7f5f570033
Ryan Vergeront <ryanvergeront@gmail.com>
2013-10-03 19:14:28 -0400

    Reduce coupling btween Anon User steps & internals

--- features/step_definitions/anonymous_users_steps.rb
+++ features/step_definitions/anonymous_users_steps.rb
@@ -6,19 +6,18 @@ When(/^I visit the site$/) do
 end
 
 When(/^give a URL to the service$/) do
-  fill_in "uri_to_shorten", with: "www.example.com/url/1"
+  fill_in "uri_to_shorten", with: "http://example.com/page"
   click_button "Shorten URL"
 end
 
 Then(/^I expect it to return a service shortened URL$/) do
-  expect(page).to have_content(/#{current_url}\/\d+\w+/)
+  shortened_urn = PersistedURI.all_short_urn.first
+  expect(page).to have_link("#{current_url}#{shortened_urn}")
 end
 
 When(/^I follow a service shortened URL$/) do
-  original_uri = "http://example.com/page"
-  short_urn = URIShortener.new.shorten(original_uri)
-  PersistedURI.new(short_urn: short_urn, long_uri: original_uri).save
-  visit "/#{short_urn}"
+  shortened_urn = PersistedURI.all_short_urn.first
+  visit "/#{shortened_urn}"
 end
 
 Then(/^I expect to be redirected to the original URL$/) do
